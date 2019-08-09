1336651c16aab1e3972b020731762baab8d51bba
RoLoDeXx <dexxrolo@gmail.com>
2019-08-09 20:09:44 +0530

    socketio refactoring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const http = require("http");
 const bodyParser = require("body-parser");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
+
+io.on("connection", () => {
+  console.log("New connected added");
+});
 
 app.get("/", (req, res) => {
   res.render("index.html");
 });
 
-app.listen(process.env.PORT);
+server.listen(process.env.PORT);
