bd421a8826a8d7d38272dcc881723ce3648aa6e3
RoLoDeXx <dexxrolo@gmail.com>
2019-08-19 22:36:16 +0530

    acknowledgments added

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -6,17 +6,31 @@ socket.on("message", welcomeMessage => {
 
 document.getElementById("submitButton").addEventListener("click", () => {
   if (document.getElementById("message").value !== "")
-    socket.emit("sendMessage", document.getElementById("message").value);
+    socket.emit(
+      "sendMessage",
+      document.getElementById("message").value,
+      res => {
+        if (res) return console.log(res);
+
+        console.log("Message delivered");
+      }
+    );
 });
 
 document.getElementById("sendLocation").addEventListener("click", () => {
   if (!navigator.geolocation) return alert("Upgrade to a newer browser");
 
   navigator.geolocation.getCurrentPosition(position => {
-    socket.emit("sendLocation", {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      res => {
+        console.log(res);
+      }
+    );
   });
 });
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const bodyParser = require("body-parser");
 const path = require("path");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app); //socketio demands
@@ -18,20 +19,27 @@ io.on("connection", socket => {
   socket.broadcast.emit("message", "Abcd has joined the chat");
 
   socket.emit("countUpdated", count);
-  socket.on("sendMessage", text => {
+  socket.on("sendMessage", (text, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(text))
+      return callback("Chat is moderated, Please do not use curse words");
+
     io.emit("message", text);
+    callback();
   });
 
   socket.on("disconnect", () => {
     io.emit("message", "User has left the chat");
   });
 
-  socket.on("sendLocation", response => {
+  socket.on("sendLocation", (response, callback) => {
     let { latitude, longitude } = response;
     io.emit(
       "message",
       "https://google.com/maps?q=" + latitude + "," + longitude
     );
+    callback("Location shared");
   });
 });
 
